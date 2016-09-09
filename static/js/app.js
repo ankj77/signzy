1d190eadae19f30680cea321d87be16bb346511e
Deepanshu <ddsr17@gmail.com>
2016-09-10 01:47:48 +0530

    minor changes

--- static/js/app.js
+++ static/js/app.js
@@ -2,11 +2,11 @@
  * Created by deepanshu on 9/9/16.
  */
 
-$("#btn1").click(function () {
-    var details = {
-        "username": $("#email-login").val(),
-        "password": $("#pwd-login").val(),
-    }
+    $("#btn1").click(function () {
+        var details = {
+            "username": $("#email-login").val(),
+            "password": $("#pwd-login").val(),
+        }
 
     // $.ajax({
     //   type: "POST",
@@ -19,53 +19,50 @@ $("#btn1").click(function () {
 
     $.post("http://localhost:8000/login/",
         {
-            "username": "ddsr17@gmail.com",
-            "password": "helloworld",
+            "username": $("#email-login").val(),
+            "password": $("#pwd-login").val()
         },
         function (response, status) {
             alert("Data: " + response.message.email + "\nStatus: " + status);
             console.log(response.message.email);
-            var dropdown = "<li class='dropdown'><a class='dropdown-toggle' data-toggle='dropdown' href='#'>"+"Hi " + response.message.email +"<span class='caret'></span></a><ul class='dropdown-menu'><li>My Account</li><li>Alumni</li><li id='logout'>Logout</li></ul></li>";
+            var dropdown = "<li class='dropdown'><a class='dropdown-toggle' data-toggle='dropdown' "+ "href='#'>"+"Hi " + response.message.email +"<span class='caret'></span></a><ul class='dropdown-menu'></ul></li>";
 
 
-            $("#currentstatus").empty();
-            $("#currentstatus").append(dropdown);
+            $("#current-login").empty();
+            $("#current-login").append(dropdown);
         });
 
-});
-
-
-$("#btn2").click(function () {
-
-    var data = {
-        "first_name": $("#firstname").val(),
-        "last_name": $("#lastname").val(),
-        "username": $("#username").val(),
-        "email": $("#email").val(),
-        "password": $("#pwd").val()
-    }
-    $.ajax({
-        type: "POST",
-        url: "http://localhost:8000/signup/",
-        data: data,
-        dataType: 'application/json'
     });
 
-});
+
+    $("#btn2").click(function () {
+
+        var data = {
+            "first_name": $("#firstname").val(),
+            "last_name": $("#lastname").val(),
+            "username": $("#username").val(),
+            "email": $("#email").val(),
+            "password": $("#pwd").val()
+        }
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8000/signup/",
+            data: data,
+            dataType: 'application/json'
+        });
+
+    });
 
 
-$('#logout').on('click',function () {
+    $('#logout').on('click',function () {
 
-    $.ajax({
-        type: "GET",
-        url: "http://localhost:8000/logout/"
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:8000/logout/"
+        }).function(response)
+            {
+                console.log(response.message)
+            };
 
-
-    }).function(response)
-    {
-        console.log(response.message
-        )
-    }
-    ;
-})
+    })
 
